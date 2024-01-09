e76f1c191a7ae9689c5afb520139388657363887
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2024-01-09 20:42:28 +0100

    Version 3.0

---
+++ boatfactory/src/main/java/org/kimfri/factories/boatfactory/BoatMain.java
@@ -0,0 +1,11 @@
+package org.kimfri.factories.boatfactory;
+
+public class BoatMain {
+  public static void main(String[] args) {
+    System.out.println("BoatFactory!!");
+  }
+
+  public void sayHello() {
+    System.out.println("BoatFactory!! v3.0");
+  }
+}
---
+++ carfactory/src/main/java/org/kimfri/factories/boatfactory/CarMain.java
@@ -0,0 +1,11 @@
+package org.kimfri.factories.boatfactory;
+
+public class CarMain {
+  public static void main(String[] args) {
+    System.out.println("CarFactory!");
+  }
+
+  public void sayHello() {
+    System.out.println("CarFactory! v3.0");
+  }
+}
